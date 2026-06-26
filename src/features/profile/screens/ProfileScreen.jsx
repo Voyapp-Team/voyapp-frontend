0c9baf60a7452e5eda60fb6f8fc3f371eed2d37f
Jaysquare <jamiuolajide20@gmail.com>
2026-06-26 14:07:43 +0100

    add styling to Profile.jsx

--- src/features/profile/screens/ProfileScreen.jsx
+++ src/features/profile/screens/ProfileScreen.jsx
@@ -2,7 +2,7 @@ import Profile from "../components/Profile"
 
 export default function ProfileScreen(){
     return(
-        <div>
+        <div className="w-full max-w-97.5 bg-[#FCF8F8] rounded-[20px] p-6 pt-10 m-auto">
           <Profile/>
         </div>
     )
